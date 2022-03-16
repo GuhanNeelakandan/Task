955b98d050a8605ec1ceb84536b2467df393aa1c
Guhan <you@example.com>
2022-03-16 15:09:09 +0530

    changes done

--- Task/Day_19_Task/pagination.js
+++ Task/Day_19_Task/pagination.js
@@ -1,4 +1,120 @@
-var tableData = [
+let div1=document.createElement('div')
+div1.setAttribute('id','div1')
+div1.classList.add('text-center','table-responsive')
+document.body.appendChild(div1)
+
+let h1=document.createElement('h1')
+h1.setAttribute('id','title')
+h1.innerHTML ='Pagination'
+h1.classList.add('h1')
+document.getElementById('div1').appendChild(h1)
+
+let div2=document.createElement('div')
+div2.setAttribute('id','list')
+document.getElementById('div1').appendChild(div2)
+
+let table = document.createElement('table');
+table.setAttribute('id','table')
+table.classList.add('table','table-bordered')
+document.getElementById('list').appendChild(table)
+
+
+let div3=document.createElement('div')
+div3.setAttribute('id','buttons')
+div3.classList.add('nav')
+div3.classList.add('d-flex','justify-content-center')
+document.getElementById('div1').appendChild(div3)
+
+let form=document.createElement('form')
+form.setAttribute('id','form')
+
+document.getElementById('buttons').appendChild(form)
+
+let input1=document.createElement('input')
+input1.setAttribute('type','button')
+input1.setAttribute('id','buttons1')
+input1.setAttribute('value','Previous')
+input1.setAttribute('onclick','previousPage()')
+document.getElementById('form').appendChild(input1)
+
+let input2=document.createElement('input')
+input2.setAttribute('type','button')
+input2.setAttribute('id','buttons2')
+input2.setAttribute('value','First')
+input2.setAttribute('onclick','Page(1)')
+document.getElementById('form').appendChild(input2)
+
+let input3=document.createElement('input')
+input3.setAttribute('type','button')
+input3.setAttribute('id','2')
+input3.setAttribute('value','2')
+input3.setAttribute('onclick','Page(2)')
+document.getElementById('form').appendChild(input3)
+
+let input4=document.createElement('input')
+input4.setAttribute('type','button')
+input4.setAttribute('id','3')
+input4.setAttribute('value','3')
+input4.setAttribute('onclick','Page(3)')
+document.getElementById('form').appendChild(input4)
+
+let input5=document.createElement('input')
+input5.setAttribute('type','button')
+input5.setAttribute('id','4')
+input5.setAttribute('value','4')
+input5.setAttribute('onclick','Page(4)')
+document.getElementById('form').appendChild(input5)
+
+let input6=document.createElement('input')
+input6.setAttribute('type','button')
+input6.setAttribute('id','5')
+input6.setAttribute('value','5')
+input6.setAttribute('onclick','Page(5)')
+document.getElementById('form').appendChild(input6)
+
+let input7=document.createElement('input')
+input7.setAttribute('type','button')
+input7.setAttribute('id','6')
+input7.setAttribute('value','6')
+input7.setAttribute('onclick','Page(6)')
+document.getElementById('form').appendChild(input7)
+
+let input8=document.createElement('input')
+input8.setAttribute('type','button')
+input8.setAttribute('id','7')
+input8.setAttribute('value','7')
+input8.setAttribute('onclick','Page(7)')
+document.getElementById('form').appendChild(input8)
+
+let input9=document.createElement('input')
+input9.setAttribute('type','button')
+input9.setAttribute('id','8')
+input9.setAttribute('value','8')
+input9.setAttribute('onclick','Page(8)')
+document.getElementById('form').appendChild(input9)
+
+let input10=document.createElement('input')
+input10.setAttribute('type','button')
+input10.setAttribute('id','9')
+input10.setAttribute('value','9')
+input10.setAttribute('onclick','Page(9)')
+document.getElementById('form').appendChild(input10)
+
+let input11=document.createElement('input')
+input11.setAttribute('type','button')
+input11.setAttribute('id','buttons3')
+input11.setAttribute('value','Last')
+input11.setAttribute('onclick','lastPage()')
+document.getElementById('form').appendChild(input11)
+
+let input12=document.createElement('input')
+input12.setAttribute('type','button')
+input12.setAttribute('id','buttons4')
+input12.setAttribute('value','Next')
+input12.setAttribute('onclick','nextPage()')
+document.getElementById('form').appendChild(input12)
+
+var data = [
  {
     "id": "1",
     "name": "Lura Senger",
@@ -501,3 +617,84 @@ var tableData = [
   }
 ]
 
+var currentPage = 1;
+  var numberPerPage = 10;
+  var numberOfPages = data.length/numberPerPage;
+
+  function previousPage() {
+    currentPage -= 1;
+    load();
+}
+
+  function nextPage() {
+      currentPage += 1;
+      load();
+  }
+  
+  function lastPage() {
+      currentPage = numberOfPages;
+      load();
+  }
+  function Page(num){
+      currentPage=num;
+      load();
+  }
+  function load() {
+      var begin = ((currentPage - 1) * numberPerPage);
+      var end = begin + numberPerPage;
+      pageList = data.slice(begin, end);
+      check();
+      List();  
+  }
+
+  let thead = document.createElement('thead');
+  thead.setAttribute('id','thead')
+
+  let row_1 = document.createElement('tr');
+  let heading_1 = document.createElement('th');
+  heading_1.innerHTML = "ID";
+  let heading_2 = document.createElement('th');
+  heading_2.innerHTML = "Name";
+  let heading_3 = document.createElement('th');
+  heading_3.innerHTML = "Email-ID";
+
+  row_1.appendChild(heading_1);
+  row_1.appendChild(heading_2);
+  row_1.appendChild(heading_3);
+  thead.appendChild(row_1);
+
+
+  function List() {
+      document.getElementById("table").innerHTML = "";
+      for (r = 0; r < pageList.length; r++) {
+        let tbody = document.createElement('tbody');
+        tbody.setAttribute('id','tbody')
+        
+        let row_2 = document.createElement('tr');
+        let row_2_data_1 = document.createElement('td');
+        row_2_data_1.innerHTML =   +pageList[r].id;;
+        let row_2_data_2 = document.createElement('td');
+        row_2_data_2.innerHTML ="Name: "+ pageList[r].name;
+        let row_2_data_3 = document.createElement('td');
+        row_2_data_3.innerHTML ="Email: "+pageList[r].email;
+        
+        row_2.appendChild(row_2_data_1);
+        row_2.appendChild(row_2_data_2);
+        row_2.appendChild(row_2_data_3);
+        tbody.appendChild(row_2);
+        table.appendChild(thead); 
+        table.appendChild(tbody);
+
+      }
+  }
+  function check() {
+      document.getElementById("buttons1").disabled = currentPage == numberOfPages ? true : false;
+      document.getElementById("buttons2").disabled = currentPage == 1 ? true : false;
+      document.getElementById("buttons3").disabled = currentPage == 1 ? true : false;
+      document.getElementById("buttons4").disabled = currentPage == numberOfPages ? true : false;
+  }
+  
+  Page();
+
+
+  
